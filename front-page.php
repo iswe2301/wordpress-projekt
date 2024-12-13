6a02552ae06b1de4deb09cd4e6ed556041da1bf6
Isa Westling <isawestling.blogg@hotmail.com>
2024-12-13 18:07:03 +0100

    Skapat dynamisk sektion för att visa sidorna "Boende" och "Upplevelser" med titel, excerpt, och länkar. Implementerat WP_Query för att hämta och loopa igenom innehåll samt lagt till stöd för utvalda bilder.

--- front-page.php
+++ front-page.php
@@ -21,6 +21,44 @@ get_header();
         }
         ?>
     </div>
+
+    <!-- Intro-sektion med länkar till boende och upplevelser -->
+    <div class="intro-content">
+        <?php
+        // Skapa en array med argument för att hämta sidor
+        $args = array(
+            "post_type" => "page", // Hämta sidor
+            "post_name__in" => array("boende", "upplevelser"), // Postnamn för boende och upplevelser
+            "posts_per_page" => 2 // Max 2 sidor
+        );
+        // Skapa en ny query med argumenten
+        $page_query = new WP_Query($args);
+        // Kontrolelra om det finns innehåll, loopa igenom och skriv ut
+        if ($page_query->have_posts()) {
+            while ($page_query->have_posts()) {
+                $page_query->the_post();
+        ?>
+                <div class="card">
+                    <!-- Kontrollera om det finns en utvald bild och visa isåfall -->
+                    <?php if (has_post_thumbnail()) { ?>
+                        <?php the_post_thumbnail(); ?>
+                    <?php } ?>
+                    <!-- Skriv ut titel och excerpt -->
+                    <h2><?php the_title(); ?></h2>
+                    <?php the_excerpt(); ?>
+                    <div class="card-buttons">
+                        <!-- Länka till sidan -->
+                        <a href="<?php the_permalink(); ?>" class="btn btn-blue">Boka nu</a>
+                        <a href="<?php the_permalink(); ?>" class="btn btn-green">Läs mer</a>
+                    </div>
+                </div>
+        <?php
+            }
+            // Återställ postdata
+            wp_reset_postdata();
+        }
+        ?>
+    </div>
 </section>
 
 <?php
