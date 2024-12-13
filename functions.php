c11d213ef4fabb0f092fca9bb2becb94a1d02c2e
Isa Westling <isawestling.blogg@hotmail.com>
2024-12-13 17:58:13 +0100

    Aktiverat stöd för widgetar, registrerat ny widget för startsidan och inaktiverat blockeditorn för widgetar

--- functions.php
+++ functions.php
@@ -12,3 +12,22 @@ function register_my_menus()
 
 // Aktivera stöd för utvalda bilder
 add_theme_support("post-thumbnails");
+
+// Aktivera widgetar
+add_action("widgets_init", "aurora_widget_init");
+
+function aurora_widget_init()
+{
+    // Widget för puff på startsidan
+    register_sidebar(array(
+        "name"          => "Puff på startsidan",
+        "id"            => "front-page-puff",
+        "before_widget" => "<div class='widget'>",
+        "after_widget"  => "</div>",
+        "before_title"  => "<h3>",
+        "after_title"   => "</h3>",
+    ));
+}
+
+// Inaktivera blockeditorn för widgetar
+add_filter("use_widgets_block_editor", "__return_false");
