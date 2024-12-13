2e5d200f055e680a96dce84fac63c521cd91c50f
Isa Westling <isawestling.blogg@hotmail.com>
2024-12-13 18:02:41 +0100

    Uppdaterat index.php för att visa inläggsinnehåll dynamiskt med loop.

--- index.php
+++ index.php
@@ -3,77 +3,21 @@
 get_header();
 ?>
 
-<section class="intro" id="intro">
-    <div class="intro-header">
-        <h1>Vad är ditt nästa äventyr?</h1>
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ullamcorper risus urna, ac
-            convallis nisl imperdiet sit amet. Aenean id efficitur neque. Suspendisse eget interdum turpis.
-        </p>
-        <div class="divider"></div>
-    </div>
-    <div class="intro-content">
-        <div class="card">
-            <img src="img/boende1.jpg" alt="Glasiglos i snölandskap">
-            <h2>Boende</h2>
-            <p>
-                Aenean nec dignissim turpis. Sed rhoncus consectetur placerat. Fusce ut ultrices neque.
-                Vestibulum quis lectus venenatis.
-            </p>
-            <div class="card-buttons">
-                <a href="#" class="btn btn-blue">Boka nu</a>
-                <a href="#" class="btn btn-green">Läs mer</a>
-            </div>
-        </div>
-        <div class="card">
-            <img src="img/upplevelser1.jpg" alt="Snöskotrar i solnedgången">
-            <h2>Upplevelser</h2>
-            <p>
-                Aenean nec dignissim turpis. Sed rhoncus consectetur placerat. Fusce ut ultrices neque.
-                Vestibulum quis lectus venenatis.
-            </p>
-            <div class="card-buttons">
-                <a href="#" class="btn btn-blue">Boka nu</a>
-                <a href="#" class="btn btn-green">Läs mer</a>
-            </div>
-        </div>
-    </div>
-</section>
-
-<div class="about">
-    <a class="btn btn-pink" href="om-oss.html">Om AurorAdventures</a>
-    <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas viverra fermentum felis vel
-        consectetur. Praesent luctus eget magna in commodo. Proin lorem nisl, finibus vitae odio aliquam,
-        vehicula malesuada est.
-    </p>
-</div>
-
-<section class="highlight">
-    <div class="highlight-content">
-        <h2>Puff</h2>
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas viverra fermentum felis vel
-            consectetur. Praesent luctus eget magna in commodo.
-        </p>
-    </div>
-</section>
-
-<div class="divider"></div>
-
-<section class="news">
-    <div class="news-card">
-        <img src="img/bubble-bath.jpg" alt="Kvinnor som badar i utomhusbadtunna">
-        <div class="news-content">
-            <h2>Senaste nytt</h2>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas viverra fermentum felis vel
-                consectetur. Praesent luctus eget magna in commodo.
-            </p>
-            <a href="#" class="btn btn-green">Läs mer</a>
-        </div>
-    </div>
-</section>
+<?php
+// Loopa igenom inlägg och visa innehållet
+if (have_posts()) {
+    while (have_posts()) {
+        the_post();
+?>
+        <section class="single-content">
+            <!-- Skriv ut titel och innehåll -->
+            <h2><?php the_title(); ?></h2>
+            <?php the_content(); ?>
+        </section>
+<?php
+    }
+}
+?>
 
 <?php
 // Inkludera footer
